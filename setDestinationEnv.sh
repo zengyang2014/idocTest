95a635fc04610f3307908f402698ae4b99e178fc
yuqixia <yuqixia@ap.xom.com>
2019-01-24 17:38:46 +0800

    [yuqing]add set env

--- setDestinationEnv.sh
+++ setDestinationEnv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export JCO_ASHOST=
-export JCO_SYSNR=
-export JCO_CLIENT=
+export JCO_ASHOST=13.76.229.211
+export JCO_SYSNR=00
+export JCO_CLIENT=001
 export JCO_USER=
 export JCO_PASSWD=
 export JCO_LANG=en
